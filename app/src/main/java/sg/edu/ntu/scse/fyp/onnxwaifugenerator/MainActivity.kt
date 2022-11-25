5d178179b4f56d432c49a08fe8446b18a159f805
acktor.sg <acktor.sg@gmail.com>
2022-11-26 02:33:28 +0800

    Ran image generation on a couroutine

--- app/src/main/java/sg/edu/ntu/scse/fyp/onnxwaifugenerator/MainActivity.kt
+++ app/src/main/java/sg/edu/ntu/scse/fyp/onnxwaifugenerator/MainActivity.kt
@@ -1,6 +1,7 @@
 package sg.edu.ntu.scse.fyp.onnxwaifugenerator
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,29 +10,52 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import sg.edu.ntu.scse.fyp.onnxwaifugenerator.ui.theme.OnnxWaifuGeneratorTheme
 
 private const val TAG = "MainActivity"
 
 class MainActivity : ComponentActivity() {
+    /**
+     * Object used to generate images from user input.
+     *
+     * lateinit = variable initialized after class instantiation
+     */
+    private lateinit var onnxGenerator: OnnxGenerator
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // OnnxGenerator initialized here because resources only accessible during and after
+        // onCreate() is called
+        onnxGenerator = OnnxGenerator(resources)
+
         setContent {
+            val scope = rememberCoroutineScope()
+
             OnnxWaifuGeneratorTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    AppUi(this::generateShape)
+                    AppUi {
+                        scope.launch {
+                            generateShape()
+                        }
+                    }
                 }
             }
         }
     }
 
-    private fun generateShape() {
-        Log.d(TAG, "generateShape: Generate shape button clicked")
+    private suspend fun generateShape() = withContext(Dispatchers.Unconfined) {
+        onnxGenerator.generateImage(0, floatArrayOf(0f, 0f), 0f)
     }
 }

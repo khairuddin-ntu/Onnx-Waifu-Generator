0cbc5f3d54cf94be3bc57e9de5d77d4933b8b57c
acktor.sg <acktor.sg@gmail.com>
2022-11-29 16:15:53 +0800

    Displayed output to screen; Moved all code to AppUi file; Fixed generation running in main thread

--- app/src/main/java/sg/edu/ntu/scse/fyp/onnxwaifugenerator/AppUi.kt
+++ app/src/main/java/sg/edu/ntu/scse/fyp/onnxwaifugenerator/AppUi.kt
@@ -1,30 +1,105 @@
 package sg.edu.ntu.scse.fyp.onnxwaifugenerator
 
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.util.Log
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
-import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlin.math.roundToInt
+
+private const val TAG = "AppUi"
 
 /**
  * Main UI
- *
- * @param generateShape Function to run when Generate button is clicked
  */
 @Composable
-fun AppUi(generateShape: () -> Unit) {
+fun AppUi() {
+    val resources = LocalContext.current.resources
+
+    val scope = rememberCoroutineScope()
+    val (onnxGenerator, _) = remember { mutableStateOf(OnnxGenerator(resources)) }
+    val (bitmap, setBitmap) = remember { mutableStateOf<Bitmap?>(null) }
+
+    // Closes Onnx Generator when UI is destroyed
+    DisposableEffect(onnxGenerator) {
+        onDispose {
+            onnxGenerator.close()
+        }
+    }
+
     Column(
         Modifier
             .fillMaxSize()
             .padding(top = 16.dp, start = 16.dp, end = 16.dp)
     ) {
-        Button(onClick = generateShape) {
+        Button(onClick = {
+            scope.launch {
+                generateShape(onnxGenerator, setBitmap)
+            }
+        }) {
             Text(text = "Generate")
+        }
+        if (bitmap != null) {
+            Image(
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                bitmap = bitmap.asImageBitmap(),
+                contentDescription = ""
+            )
         }
     }
 }
+
+/**
+ * Performs shape generation in a background thread
+ *
+ * @param onnxGenerator Object used to generate images from user input
+ * @param setBitmap Function to update Bitmap in UI
+ */
+private suspend fun generateShape(onnxGenerator: OnnxGenerator, setBitmap: (Bitmap) -> Unit) =
+    withContext(Dispatchers.Default) {
+        val (modelOutput, shape) = onnxGenerator.generateImage(0, floatArrayOf(0f, 0f), 0f)
+
+        Log.d(TAG, "generateShape: Output shape = ${shape.joinToString()}")
+        Log.d(TAG, "generateShape: Converting model output to Bitmap")
+        val imgWidth = shape[3].toInt()
+        val imgHeight = shape[2].toInt()
+
+        val minVal = modelOutput.minOrNull() ?: -1.0f
+        val maxVal = modelOutput.maxOrNull() ?: 1.0f
+        val delta = maxVal - minVal
+
+        val pixels = IntArray(imgWidth * imgHeight * 4)
+        for (i in 0 until imgWidth * imgHeight) {
+            pixels[i] = Color.rgb(
+                ((modelOutput[i] - minVal) / delta * 255.0f).roundToInt(),
+                ((modelOutput[i + imgWidth * imgHeight] - minVal) / delta * 255.0f).roundToInt(),
+                ((modelOutput[i + 2 * imgWidth * imgHeight] - minVal) / delta * 255.0f).roundToInt()
+            )
+        }
+
+        val bitmap = Bitmap.createBitmap(imgWidth, imgHeight, Bitmap.Config.ARGB_8888)
+        bitmap.setPixels(pixels, 0, imgWidth, 0, 0, imgWidth, imgHeight)
+        setBitmap(bitmap)
+
+        // Save to file in app storage
+//        val fileName = "model-output-${System.currentTimeMillis()}.png"
+//        val file = File(applicationContext.filesDir, fileName)
+//        bitmap.compress(Bitmap.CompressFormat.PNG, 100, file.outputStream())
+//        Log.d(
+//            TAG,
+//            "generateShape: Bitmap stored in ${applicationContext.filesDir.absolutePath}/$fileName"
+//        )
+    }
--- app/src/main/java/sg/edu/ntu/scse/fyp/onnxwaifugenerator/MainActivity.kt
+++ app/src/main/java/sg/edu/ntu/scse/fyp/onnxwaifugenerator/MainActivity.kt
@@ -1,93 +1,28 @@
 package sg.edu.ntu.scse.fyp.onnxwaifugenerator
 
-import android.graphics.Bitmap
-import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import sg.edu.ntu.scse.fyp.onnxwaifugenerator.ui.theme.OnnxWaifuGeneratorTheme
-import java.io.File
-import kotlin.math.roundToInt
-
-private const val TAG = "MainActivity"
 
 class MainActivity : ComponentActivity() {
-    /**
-     * Object used to generate images from user input.
-     *
-     * lateinit = variable initialized after class instantiation
-     */
-    private lateinit var onnxGenerator: OnnxGenerator
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // OnnxGenerator initialized here because resources only accessible during and after
-        // onCreate() is called
-        onnxGenerator = OnnxGenerator(resources)
-
         setContent {
-            val scope = rememberCoroutineScope()
-
             OnnxWaifuGeneratorTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    AppUi {
-                        scope.launch {
-                            generateShape()
-                        }
-                    }
+                    AppUi()
                 }
             }
         }
-    }
-
-    override fun onDestroy() {
-        onnxGenerator.close()
-        super.onDestroy()
-    }
-
-    private suspend fun generateShape() = withContext(Dispatchers.Unconfined) {
-        val (modelOutput, shape) = onnxGenerator.generateImage(0, floatArrayOf(0f, 0f), 0f)
-
-        Log.d(TAG, "generateShape: Output shape = ${shape.joinToString()}")
-        Log.d(TAG, "generateShape: Converting model output to Bitmap")
-        val imgWidth = shape[3].toInt()
-        val imgHeight = shape[2].toInt()
-
-        val minVal = modelOutput.minOrNull() ?: -1.0f
-        val maxVal = modelOutput.maxOrNull() ?: 1.0f
-        val delta = maxVal - minVal
-
-        val pixels = IntArray(imgWidth * imgHeight * 4)
-        for (i in 0 until imgWidth * imgHeight) {
-            val r = ((modelOutput[i] - minVal) / delta * 255.0f).roundToInt();
-            val g = ((modelOutput[i + imgWidth * imgHeight] - minVal) / delta * 255.0f).roundToInt()
-            val b = ((modelOutput[i + 2 * imgWidth * imgHeight] - minVal) / delta * 255.0f).roundToInt()
-            pixels[i] = Color.rgb(r, g, b)
-        }
-
-        val bitmap = Bitmap.createBitmap(imgWidth, imgHeight, Bitmap.Config.ARGB_8888)
-        bitmap.setPixels(pixels, 0, imgWidth, 0, 0, imgWidth, imgHeight)
-
-        val fileName = "model-output-${System.currentTimeMillis()}.png"
-        val file = File(applicationContext.filesDir, fileName)
-        bitmap.compress(Bitmap.CompressFormat.PNG, 100, file.outputStream())
-        Log.d(
-            TAG,
-            "generateShape: Bitmap stored in ${applicationContext.filesDir.absolutePath}/$fileName"
-        )
     }
 }

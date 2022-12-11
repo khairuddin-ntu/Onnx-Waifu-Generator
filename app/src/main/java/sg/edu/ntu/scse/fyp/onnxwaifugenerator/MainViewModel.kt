754fff3c85abdd00cfae2cfb0d79937d00ea86e0
acktor.sg <acktor.sg@gmail.com>
2022-12-11 21:27:03 +0800

    Created ViewModel class for main UI

---
+++ app/src/main/java/sg/edu/ntu/scse/fyp/onnxwaifugenerator/MainViewModel.kt
@@ -0,0 +1,8 @@
+package sg.edu.ntu.scse.fyp.onnxwaifugenerator
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class MainViewModel(app: Application) : AndroidViewModel(app) {
+    
+}

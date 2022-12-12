ebe10f9e271fce2b72e85513e67166fcf161b38f
acktor.sg <acktor.sg@gmail.com>
2022-12-12 17:01:13 +0800

    Migrated UI widgets to Material 3

--- app/src/main/java/sg/edu/ntu/scse/fyp/onnxwaifugenerator/MainUi.kt
+++ app/src/main/java/sg/edu/ntu/scse/fyp/onnxwaifugenerator/MainUi.kt
@@ -4,7 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
-import androidx.compose.material.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier

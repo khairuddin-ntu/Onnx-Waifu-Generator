41c0f5f2070fe8fe568037c786131c93f32cc243
acktor.sg <acktor.sg@gmail.com>
2022-12-13 23:35:43 +0800

    Created dropdown menu for model

---
+++ app/src/main/java/sg/edu/ntu/scse/fyp/onnxwaifugenerator/ModelSelector.kt
@@ -0,0 +1,47 @@
+package sg.edu.ntu.scse.fyp.onnxwaifugenerator
+
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ModelSelector(
+    selectedModel: OnnxModel,
+    setModel: (OnnxModel) -> Unit
+) {
+    val options = OnnxModel.values()
+    var expanded by remember { mutableStateOf(false) }
+
+    // We want to react on tap/press on TextField to show menu
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = { expanded = !expanded },
+    ) {
+        TextField(
+            // The `menuAnchor` modifier must be passed to the text field for correctness.
+            modifier = Modifier.menuAnchor(),
+            readOnly = true,
+            value = selectedModel.label,
+            onValueChange = {},
+            label = { Text("Model") },
+            trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+            colors = ExposedDropdownMenuDefaults.textFieldColors(),
+        )
+        ExposedDropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+        ) {
+            options.forEach { selectionOption ->
+                DropdownMenuItem(
+                    text = { Text(selectionOption.label) },
+                    onClick = {
+                        setModel(selectionOption)
+                        expanded = false
+                    },
+                    contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding,
+                )
+            }
+        }
+    }
+}

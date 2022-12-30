b51de65deccdf03e7f7edc7225a871c6046b7acd
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2022-12-30 14:28:51 +0800

    Fixed missing model in form state

--- app/src/main/java/sg/edu/ntu/scse/fyp/onnxwaifugenerator/form/FormData.kt
+++ app/src/main/java/sg/edu/ntu/scse/fyp/onnxwaifugenerator/form/FormData.kt
@@ -1,8 +1,10 @@
 package sg.edu.ntu.scse.fyp.onnxwaifugenerator.form
 
 import androidx.compose.runtime.saveable.mapSaver
+import sg.edu.ntu.scse.fyp.onnxwaifugenerator.common.OnnxModel
 
 data class FormData(
+    val model: OnnxModel,
     val seed: Int,
     val isRandomSeed: Boolean,
     val trunc1: Float,
@@ -11,6 +13,7 @@ data class FormData(
 )
 
 val FormSaver = run {
+    val modelKey= "model"
     val seedKey = "seed"
     val randomSeedKey = "random_seed"
     val trunc1Key = "trunc1"
@@ -20,6 +23,7 @@ val FormSaver = run {
     mapSaver(
         save = {
             mapOf(
+                modelKey to it.model.name,
                 seedKey to it.seed,
                 randomSeedKey to it.isRandomSeed,
                 trunc1Key to it.trunc1,
@@ -29,6 +33,7 @@ val FormSaver = run {
         },
         restore = {
             FormData(
+                OnnxModel.valueOf(it[modelKey] as String),
                 it[seedKey] as Int,
                 it[randomSeedKey] as Boolean,
                 it[trunc1Key] as Float,

bbdb91ceab1295988103a44e55fbc5ad5af1f270
acktor.sg <acktor.sg@gmail.com>
2022-12-30 21:19:25 +0800

    Created template for starting image generation service

--- app/src/main/java/sg/edu/ntu/scse/fyp/onnxwaifugenerator/MainActivity.kt
+++ app/src/main/java/sg/edu/ntu/scse/fyp/onnxwaifugenerator/MainActivity.kt
@@ -1,5 +1,6 @@
 package sg.edu.ntu.scse.fyp.onnxwaifugenerator
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,11 +8,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import sg.edu.ntu.scse.fyp.onnxwaifugenerator.onnxgeneration.ImageGenerationService
 import sg.edu.ntu.scse.fyp.onnxwaifugenerator.ui.theme.OnnxWaifuGeneratorTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var serviceIntent: Intent
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        serviceIntent = Intent(this, ImageGenerationService::class.java)
+        // TODO: Change to foreground service
+        startService(serviceIntent)
 
         setContent {
             OnnxWaifuGeneratorTheme {
@@ -24,5 +32,10 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    override fun onDestroy() {
+        stopService(serviceIntent)
+        super.onDestroy()
     }
 }
---
+++ app/src/main/java/sg/edu/ntu/scse/fyp/onnxwaifugenerator/onnxgeneration/ImageGenerationService.kt
@@ -0,0 +1,25 @@
+package sg.edu.ntu.scse.fyp.onnxwaifugenerator.onnxgeneration
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+
+private const val TAG = "ImageGenerationService"
+
+class ImageGenerationService : Service() {
+    override fun onCreate() {
+        Log.d(TAG, "++onCreate++")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return START_STICKY
+    }
+
+    override fun onBind(intent: Intent?): IBinder? = null
+
+    override fun onDestroy() {
+        Log.d(TAG, "++onDestroy++")
+        super.onDestroy()
+    }
+}

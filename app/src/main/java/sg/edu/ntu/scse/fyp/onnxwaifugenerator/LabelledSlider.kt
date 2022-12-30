75116728c3c549fe90ab572598d48edd6986eb87
acktor.sg <acktor.sg@gmail.com>
2022-12-30 09:15:38 +0800

    Created preview of sliders

--- app/src/main/java/sg/edu/ntu/scse/fyp/onnxwaifugenerator/LabelledSlider.kt
+++ app/src/main/java/sg/edu/ntu/scse/fyp/onnxwaifugenerator/LabelledSlider.kt
@@ -1,11 +1,15 @@
 package sg.edu.ntu.scse.fyp.onnxwaifugenerator
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun SeedSlider(
@@ -46,4 +50,30 @@ fun FloatParamSlider(
         steps = (maxValue / 0.1f).toInt() - 1,
         enabled = isEnabled
     )
+}
+
+@Preview
+@Composable
+fun SeedSliderPreview() {
+    Column(
+        Modifier.padding(top = 16.dp, start = 16.dp, end = 16.dp)
+    ) {
+        SeedSlider(seedValue = 0, setSeed = {}, isEnabled = true)
+    }
+}
+
+@Preview
+@Composable
+fun FloatSliderPreview() {
+    Column(
+        Modifier.padding(top = 16.dp, start = 16.dp, end = 16.dp)
+    ) {
+        FloatParamSlider(
+            label = "Truncation 1",
+            value = 0f,
+            maxValue = 2f,
+            onValueChange = {},
+            isEnabled = true
+        )
+    }
 }

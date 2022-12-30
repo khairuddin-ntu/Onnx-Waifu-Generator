3d1c474715fe1e1810c9a9c0f8ed7d351d05cc99
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2022-12-30 12:15:22 +0800

    Moved model selector to form UI

--- app/src/main/java/sg/edu/ntu/scse/fyp/onnxwaifugenerator/GeneratorForm.kt
+++ app/src/main/java/sg/edu/ntu/scse/fyp/onnxwaifugenerator/GeneratorForm.kt
@@ -3,6 +3,16 @@ package sg.edu.ntu.scse.fyp.onnxwaifugenerator
 import androidx.compose.runtime.Composable
 
 @Composable
-fun GeneratorForm() {
+fun GeneratorForm(
+    model: OnnxModel,
+    setModel: (OnnxModel) -> Unit,
+    isGenerating: Boolean
+) {
+    ModelSelector(
+        selectedModel = model,
+        setModel = setModel,
+        enabled = !isGenerating
+    )
+}
 
 }
--- app/src/main/java/sg/edu/ntu/scse/fyp/onnxwaifugenerator/MainUi.kt
+++ app/src/main/java/sg/edu/ntu/scse/fyp/onnxwaifugenerator/MainUi.kt
@@ -62,11 +62,9 @@ fun MainUi(mainViewModel: MainViewModel = viewModel()) {
             .fillMaxSize()
             .padding(top = 16.dp, start = 16.dp, end = 16.dp)
     ) {
-        GeneratorForm()
-        ModelSelector(
-            selectedModel = model,
-            setModel = setModel,
-            enabled = !isGenerating
+        GeneratorForm(
+            model, setModel,
+            isGenerating
         )
         Spacer(Modifier.padding(bottom = 16.dp))
         SeedSlider(

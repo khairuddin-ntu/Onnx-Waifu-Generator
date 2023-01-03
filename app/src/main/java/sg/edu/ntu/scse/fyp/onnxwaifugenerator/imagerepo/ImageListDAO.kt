d3bf3d51ec891c198605952e04f1ef0ce8fb5813
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2023-01-03 18:58:39 +0800

    Created function to convert list of file paths into File objects

--- app/src/main/java/sg/edu/ntu/scse/fyp/onnxwaifugenerator/imagerepo/ImageListDAO.kt
+++ app/src/main/java/sg/edu/ntu/scse/fyp/onnxwaifugenerator/imagerepo/ImageListDAO.kt
@@ -3,9 +3,13 @@ package sg.edu.ntu.scse.fyp.onnxwaifugenerator.imagerepo
 import androidx.room.Dao
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import java.io.File
 
 @Dao
-interface ImageListDAO {
+abstract class ImageListDAO {
     @Query("SELECT filePath FROM RenderedImage")
-    fun getAllImagePaths(): Flow<List<String>>
+    protected abstract fun getAllImagePaths(): Flow<List<String>>
+
+    fun getAllImages() = getAllImagePaths().map { it.map(::File) }
 }

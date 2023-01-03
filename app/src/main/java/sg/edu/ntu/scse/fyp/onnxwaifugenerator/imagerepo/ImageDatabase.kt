06a7defefc7d4cd10dec99333843364a96ae0753
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2023-01-03 18:46:34 +0800

    Created database for generated images

---
+++ app/src/main/java/sg/edu/ntu/scse/fyp/onnxwaifugenerator/imagerepo/ImageDatabase.kt
@@ -0,0 +1,8 @@
+package sg.edu.ntu.scse.fyp.onnxwaifugenerator.imagerepo
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(version = 1, entities = [RenderedImage::class])
+abstract class ImageDatabase : RoomDatabase() {
+}

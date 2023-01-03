a69f3086b46d34267217fe0c9a0c31091d8e2268
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2023-01-03 19:37:27 +0800

    Reverted conversion of file paths to File objects

--- app/src/main/java/sg/edu/ntu/scse/fyp/onnxwaifugenerator/imagerepo/ImageListDAO.kt
+++ app/src/main/java/sg/edu/ntu/scse/fyp/onnxwaifugenerator/imagerepo/ImageListDAO.kt
@@ -3,13 +3,9 @@ package sg.edu.ntu.scse.fyp.onnxwaifugenerator.imagerepo
 import androidx.room.Dao
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
-import java.io.File
 
 @Dao
-abstract class ImageListDAO {
+interface ImageListDAO {
     @Query("SELECT filePath FROM RenderedImage")
-    protected abstract fun getAllImagePaths(): Flow<List<String>>
-
-    fun getAllImages() = getAllImagePaths().map { it.map(::File) }
+    fun getAllImagePaths(): Flow<List<String>>
 }

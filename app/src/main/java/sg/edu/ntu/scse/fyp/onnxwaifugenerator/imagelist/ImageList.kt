53c80cbea979b1eecbb23ea36633c85338c17e08
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2023-01-08 22:30:59 +0800

    Converted horizontal pager to vertical grid

--- app/src/main/java/sg/edu/ntu/scse/fyp/onnxwaifugenerator/imagelist/ImageList.kt
+++ app/src/main/java/sg/edu/ntu/scse/fyp/onnxwaifugenerator/imagelist/ImageList.kt
@@ -4,8 +4,11 @@ import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material3.FloatingActionButton
@@ -14,31 +17,19 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.dimensionResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.rememberAsyncImagePainter
-import com.google.accompanist.pager.ExperimentalPagerApi
-import com.google.accompanist.pager.HorizontalPager
-import com.google.accompanist.pager.rememberPagerState
 import kotlinx.coroutines.flow.collectLatest
 import sg.edu.ntu.scse.fyp.onnxwaifugenerator.R
 import java.io.File
 
 private const val TAG = "ImageList"
 
-@OptIn(ExperimentalPagerApi::class)
 @Composable
 fun ImageList(listViewModel: ImageListViewModel = viewModel(), launchForm: () -> Unit) {
     var images by rememberSaveable { mutableStateOf<List<File>>(emptyList()) }
-    val pagerState = rememberPagerState()
-
-    // Updates on first launch & whenever generatedImages is updated
-    LaunchedEffect(images) {
-        pagerState.scrollToPage(
-            if (images.isEmpty()) 0
-            else images.lastIndex
-        )
-    }
 
     LaunchedEffect(Unit) {
         listViewModel.imageList.collectLatest { newList ->
@@ -48,17 +39,21 @@ fun ImageList(listViewModel: ImageListViewModel = viewModel(), launchForm: () ->
     }
 
     Box {
-        HorizontalPager(
+        LazyVerticalGrid(
             modifier = Modifier.fillMaxSize(),
-            count = images.size,
-            state = pagerState
-        ) { i ->
-            val painter = rememberAsyncImagePainter(images[i])
-            Image(
-                modifier = Modifier.fillMaxWidth(),
-                painter = painter,
-                contentDescription = ""
-            )
+            columns = GridCells.Fixed(3)
+        ) {
+            items(
+                items = images,
+                key = File::getAbsolutePath
+            ) { image ->
+                val painter = rememberAsyncImagePainter(image)
+                Image(
+                    modifier = Modifier.size(with(LocalDensity.current) { 512.toDp() }),
+                    painter = painter,
+                    contentDescription = ""
+                )
+            }
         }
         FloatingActionButton(
             onClick = launchForm,
